3782ef9316411e289abb588840a9f5334dcf0443
Charles Alves <charles.siqueira@gmail.com>
2020-12-07 19:36:11 -0300

    test: configurando a aplicação para que seja possível realizar testes unitários

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   env: {
     es2021: true,
-    node: true
+    node: true,
+    'jest/globals': true
   },
   extends: [
     'standard'
@@ -11,5 +12,11 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
-  }
+    'jest/no-disabled-tests': 'warn',
+    'jest/no-focused-tests': 'error',
+    'jest/no-identical-title': 'error',
+    'jest/prefer-to-have-length': 'warn',
+    'jest/valid-expect': 'error'
+  },
+  plugins: ['jest']
 }
