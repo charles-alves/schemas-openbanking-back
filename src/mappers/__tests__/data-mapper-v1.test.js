2a802f6a0712853f2320bd17cdb0ef16caae9bf8
Charles Alves <charles.siqueira@gmail.com>
2020-12-11 09:57:15 -0300

    test: adicionando uma situação mais complexa aos testes de data-mapper-v1

--- src/mappers/__tests__/data-mapper-v1.test.js
+++ src/mappers/__tests__/data-mapper-v1.test.js
@@ -1,4 +1,5 @@
 import { dataMapperV1 } from '../data-mapper-v1.js'
+import { dataMapper } from '../data-mapper.js'
 
 describe('dataMapperV1 test', () => {
   it('should resturn an empty object if the array is also empty', () => {
@@ -200,6 +201,147 @@ describe('dataMapperV1 test', () => {
             observation: '',
             minOccurrences: 1,
             maxOccurrences: 33
+          }
+        }
+      }
+    })
+  })
+
+  it('should process a complex structure', () => {
+    const obj = dataMapperV1.createObject([
+      {
+        name: 'brand',
+        level: 0,
+        description: '',
+        fieldType: '',
+        size: 0,
+        required: false,
+        regexValidation: '',
+        allowedValues: [],
+        minOccurrences: '-',
+        maxOccurrences: '-',
+        observation: ''
+      },
+      {
+        name: 'name',
+        level: 1,
+        description: 'Nome da Marca reportada pelo participante do Open Banking.',
+        fieldType: 'Texto',
+        size: 30,
+        required: true,
+        regexValidation: '\\w*\\W*',
+        allowedValues: [],
+        minOccurrences: '1',
+        maxOccurrences: '33',
+        observation: ''
+      },
+      {
+        name: 'loans',
+        level: 1,
+        description: '',
+        fieldType: '',
+        size: 0,
+        required: false,
+        regexValidation: '',
+        allowedValues: [],
+        minOccurrences: '',
+        maxOccurrences: '',
+        observation: ''
+      },
+      {
+        name: 'type',
+        level: 2,
+        description: 'Type of loan',
+        fieldType: 'Texto',
+        size: 30,
+        required: true,
+        regexValidation: '\\w\\W',
+        allowedValues: ['CHEQUE-ESPECIAL', 'CONSIGNADO', 'FINANCIAMENTO_IMOVEL'],
+        minOccurrences: '32',
+        maxOccurrences: '32',
+        observation: ''
+      },
+      {
+        name: 'cpnj',
+        level: 1,
+        description: 'Company CNPJ',
+        fieldType: 'Texto',
+        size: 14,
+        required: true,
+        regexValidation: '\\w*\\W*',
+        allowedValues: [],
+        minOccurrences: '1',
+        maxOccurrences: '33',
+        observation: ''
+      }
+    ])
+
+    expect(obj).toEqual({
+      brand: {
+        meta: {
+          fieldType: 'List',
+          required: true,
+          minOccurrences: 1,
+          maxOccurrences: 33
+        },
+        name: {
+          meta: {
+            fieldType: 'String',
+            description: 'Nome da Marca reportada pelo participante do Open Banking.',
+            size: 30,
+            required: true,
+            validation: '\\w*\\W*',
+            allowedValues: [],
+            observation: '',
+            minOccurrences: 1,
+            maxOccurrences: 33
+          }
+        },
+        loans: {
+          meta: {
+            fieldType: 'List',
+            required: true,
+            minOccurrences: 32,
+            maxOccurrences: 32
+          },
+          type: {
+            meta: {
+              fieldType: 'Enum',
+              description: 'Type of loan',
+              size: 30,
+              required: true,
+              validation: '\\w\\W',
+              allowedValues: [
+                {
+                  enum: null,
+                  value: 'CHEQUE-ESPECIAL'
+                },
+                {
+                  enum: 'CONSIGNADO',
+                  value: 'CONSIGNADO'
+                },
+                {
+                  enum: 'FINANCIAMENTO_IMOVEL',
+                  value: 'FINANCIAMENTO_IMOVEL'
+                }
+              ],
+              observation: '',
+              minOccurrences: 32,
+              maxOccurrences: 32
+            }
+          }
+        },
+        cpnj: {
+          meta: {
+            description: 'Company CNPJ',
+            fieldType: 'String',
+            size: 14,
+            required: true,
+            validation: '\\w*\\W*',
+            allowedValues: [],
+            minOccurrences: 1,
+            maxOccurrences: 33,
+            observation: ''
           }
         }
       }
